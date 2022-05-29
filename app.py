ab05a5479d85ba7d3a8c022a6e18b771087458a7
Reena R <82110193+Reena2001@users.noreply.github.com>
2022-05-29 13:52:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-
 from flask import Flask, render_template,request
-import pymongo
-import urllib
-
-mongo = pymongo.MongoClient('mongodb+srv://22_Omprakash:'+urllib.parse.quote("Opsr@2410")+'@invoice.wu80e.mongodb.net/Invoice?retryWrites=true&w=majority', tls=True, tlsAllowInvalidCertificates=True)
-db = pymongo.database.Database(mongo, 'Task')
-col = pymongo.collection.Collection(db, 'taskdata')
-
 app=Flask(__name__)
-
-
 @app.route('/api/branch')
 def fun():
     q=request.form.get('q')
@@ -19,9 +9,5 @@ def fun():
     for i in x:
         print(i)
     return 'success'
-    # a={"branches": [{"ifsc": "ABNA0000001","bank_id": "110","branch": "RTGS-HO",      "address": "414 EMPIRE COMPLEX, SENAPATI BAPAT MARG LOWER PAREL WEST MUMBAI 400013","city": "MUMBAI","district": "GREATER BOMBAY","state": "MAHARASHTRA"}]}
-    # return a
-
-
-if __name__=='__main__':
-    app.run(debug=True)
+    if __name__=='__main__':
+        app.run(debug=True)
